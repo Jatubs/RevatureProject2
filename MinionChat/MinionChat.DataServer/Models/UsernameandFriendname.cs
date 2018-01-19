dfbd228ccc6750e26a8e4a4d43d3c5164ca848e6
yoon kim <yoondukkim7@gmail.com>
2018-01-19 10:51:24 -0500

    added addfriend function

---
+++ MinionChat/MinionChat.DataServer/Models/UsernameandFriendname.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MinionChat.DataServer.Models
+{
+    public class UsernameandFriendname
+    {
+        public string Username { get; set; }
+        public string Friendname { get; set; }
+    }
+}
